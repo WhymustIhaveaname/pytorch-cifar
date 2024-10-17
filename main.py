6db4c95710d8e70518ea73f044ec938b0dff53d9
Zenbook <syouran0508@gmail.com>
2024-10-17 22:14:18 +0800

    add subtractresnet

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python3
+# pylint: disable=C0116,C0103,C0115,C0209,E1101,W1514
+
 '''Train CIFAR10 with PyTorch.'''
 import torch
 import torch.nn as nn
@@ -24,6 +27,7 @@ args = parser.parse_args()
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 best_acc = 0  # best test accuracy
 start_epoch = 0  # start from epoch 0 or last checkpoint epoch
+accus = [] # accuracies for plot
 
 # Data
 print('==> Preparing data..')
@@ -68,7 +72,8 @@ print('==> Building model..')
 # net = ShuffleNetV2(1)
 # net = EfficientNetB0()
 # net = RegNetX_200MF()
-net = SimpleDLA()
+# net = SimpleDLA()
+net = SubtractResNet18()
 net = net.to(device)
 if device == 'cuda':
     net = torch.nn.DataParallel(net)
@@ -82,12 +87,13 @@ if args.resume:
     net.load_state_dict(checkpoint['net'])
     best_acc = checkpoint['acc']
     start_epoch = checkpoint['epoch']
+    accus = checkpoint['accus'] if 'accus' in checkpoint else []
+    print("Resuming from epoch %d with test acc %.2f%%"%(start_epoch,best_acc))
 
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(net.parameters(), lr=args.lr,
                       momentum=0.9, weight_decay=5e-4)
 scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=200)
-
 
 # Training
 def train(epoch):
@@ -109,8 +115,9 @@ def train(epoch):
         total += targets.size(0)
         correct += predicted.eq(targets).sum().item()
 
-        progress_bar(batch_idx, len(trainloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
-                     % (train_loss/(batch_idx+1), 100.*correct/total, correct, total))
+        if batch_idx%10==0:
+            progress_bar(batch_idx, len(trainloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
+                         % (train_loss/(batch_idx+1), 100.*correct/total, correct, total))
 
 
 def test(epoch):
@@ -130,17 +137,22 @@ def test(epoch):
             total += targets.size(0)
             correct += predicted.eq(targets).sum().item()
 
-            progress_bar(batch_idx, len(testloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
-                         % (test_loss/(batch_idx+1), 100.*correct/total, correct, total))
+            if (batch_idx+1)%10==0:
+                progress_bar(batch_idx, len(testloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
+                             % (test_loss/(batch_idx+1), 100.*correct/total, correct, total))
 
     # Save checkpoint.
     acc = 100.*correct/total
+    accus.append(acc)
     if acc > best_acc:
-        print('Saving..')
+        print('Saving...')
         state = {
             'net': net.state_dict(),
             'acc': acc,
             'epoch': epoch,
+            'optimizer': optimizer.state_dict(),
+            'scheduler': scheduler.state_dict(),
+            'accus': accus
         }
         if not os.path.isdir('checkpoint'):
             os.mkdir('checkpoint')
@@ -148,7 +160,7 @@ def test(epoch):
         best_acc = acc
 
 
-for epoch in range(start_epoch, start_epoch+200):
+for epoch in range(start_epoch, 200):
     train(epoch)
     test(epoch)
     scheduler.step()
